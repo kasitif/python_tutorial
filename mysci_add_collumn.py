29e0569a33dd54c220d68bca07d587fb9a8e49fa
Kasitif <kasitif@gmail.com>
2022-02-21 11:56:40 +0300

    Refactor data parsing code

--- mysci_add_collumn.py
+++ mysci_add_collumn.py
@@ -1,29 +1,61 @@
-## Initaialize the data
-data = {'date' : [],
-        'time': [],
-        'tempout': []}
+# ## Initaialize the data
+# data = {'date' : [],
+#         'time': [],
+#         'tempout': []}
 
 
-# Read and Parse the data
+# # Read and Parse the data
+# filename = 'data\\wxobs20170821.txt'
+# with open(filename, 'r') as datafile:
+
+#     ##read the firth three lines(headers)
+#     for _ in range(3):
+#         datafile.readline()
+
+
+#     #read and parse the rest of the file
+#     for line in datafile:
+#         split_line = line.split()
+#         data['date'].append(split_line[0])
+#         data['time'].append(split_line[1])
+#         #change the line to float
+#         data['tempout'].append(float(split_line[2]))
+
+# ## convert the data to float
+
+# ##Debug
+# print(data['tempout'])
+
+## column names and column indices
+columns = {'date':0, 'time':1, 'tempout':2}
+
+# Data types for each column (only if non-string)
+types = {'tempout': float}
+
+#inituialise my data variables
+data = {}
+for column in columns:
+    data[column] = []
+
+
+
+#Reand Parse the data
 filename = 'data\\wxobs20170821.txt'
-with open(filename, 'r') as datafile:
+with open(filename,'r') as datafile:
 
     ##read the firth three lines(headers)
     for _ in range(3):
         datafile.readline()
 
 
-    #read and parse the rest of the file
+    #Read and Parse the rest of the file
     for line in datafile:
-        split_line = line.split()
-        data['date'].append(split_line[0])
-        data['time'].append(split_line[1])
-        #change the line to float
-        data['tempout'].append(float(split_line[2]))
+        split_line =line.split()
+        for column in columns:
+            i = columns[column]
+            t = types.get(column,str)
+            value =t(split_line[i])
+            data[column].append(value)
 
-## convert the data to float
-
-##Debug
+#debug
 print(data['tempout'])
-
-    
