a9b4900b616890a057833a1edf69020444e6580c
Kasitif <kasitif@gmail.com>
2022-02-23 13:16:39 +0300

    Compute dew2 point

--- dewpointinterpcop.py
+++ dewpointinterpcop.py
@@ -1,20 +1,21 @@
-from readdata import read_data
-from printing import print_comparison
-from computation import  compute_windchill
+from mysci.readdata import read_data
+from mysci.printing import print_comparison
+from mysci.computation import  compute_dewpoint
 
 #column name and indices
-columns = {'date': 0, 'time': 1, 'tempout':2,'windspeed':7, 'windchill':12 }
+columns = {'date': 0, 'time': 1, 'tempout':2,'humout':5, 'dewpt':6 }
 
 #data types for ach cilumn
-types = {'tempout':float, 'windspeed':float, 'windchill':float}
+types = {'tempout':float, 'humout':float, 'dewpt':float}
 
 #read data fro file
 data = read_data(columns, types=types)
 
 #comoute the will chill factor
-windchill = []
-for temp, windspeed in zip(data['tempout'], data['windspeed']):
-    windchill.append(compute_windchill(temp, windspeed))
+# windchill = []
+# for temp, windspeed in zip(data['tempout'], data['windspeed']):
+#     windchill.append(compute_windchill(temp, windspeed))
 
+dewpointtemp = [compute_dewpoint(t,h) for t, h in zip(data['tempout'], data['humout'])]
 ##output comparison
-print_comparison('WINDCHILL', data['date'], data['time'], data['windchill'], windchill)
+print_comparison('DEW PT', data['date'], data['time'], data['dewpt'], dewpointtemp)
